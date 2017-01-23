fc556028feb635393b356271dd46a104ce536fb1
Matthias Andriessen <kzenmatthias@Mattbook-Pro.local>
2017-01-23 22:21:02 +0100

    learned to stuff

--- myCtrl.js
+++ myCtrl.js
@@ -1,8 +1,28 @@
-app.controller("myCtrl", function($scope) {
+app.controller("myCtrl", function($scope, $http) {
 
 	$scope.firstName = "Matthias";
 	$scope.lastName = "Andriessen";
+	$scope.changeContent = function() {
+		$scope.firstName = "Jefke";
+	};
 
+	$scope.data = $http
+		.get("http://www.w3schools.com/angular/customers.php")
+		.then(
+			function(response) {
+				$scope.data = response.data.records;
+			},
+			function(response) {
+				console.log("something went wrong");
+			}
+		);
+
+});
+
+app.filter('myFilter', function() {
+	return function(x) {
+		return x.toUpperCase();
+	};
 });
 
 app.directive("matthiasTestDirective", function() {
